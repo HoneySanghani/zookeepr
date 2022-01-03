7e6b5305e908c29415884ff80d903aed35906e27
Honey Sanghani <honeysanghani98@gmail.com>
2022-01-03 15:59:38 -0500

    post request added

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const fs=require('fs');
+const path=require('path');
 const {animals}=require('./data/animals.json');
 const express=require('express');
 const { json } = require('express');
@@ -5,6 +7,10 @@ const { json } = require('express');
 const PORT=process.env.PORT||3002;
 const app=express();
 
+//parse incoming string or array data
+app.use(express.urlencoded({extended:true}));
+//parse incoming json data
+app.use(express.json());
 function filterByQuery(query,animalArray){
   let personalityTraitsArray=[];
   let filteredResults=animalArray;
@@ -33,9 +39,38 @@ function filterByQuery(query,animalArray){
   }
   return filteredResults;
 }
+
 function findById(id,animalArray){
   const result=animalArray.filter(animal=>animal.id===id)[0];
   return result;
+}
+
+function createNewAnimal(body,animalsArray){
+  const animal=body;
+  animalsArray.push(animal);
+  fs.writeFileSync(
+    path.join(__dirname,'./data/animals.json'),
+    JSON.stringify({animals:animalsArray},null,2)
+  );
+  return animal;
+}
+
+function validateAnimal(animal){
+  if(!animal.name||typeof animal.name!=='string'){
+    return false;
+  }
+  if(!animal.species||typeof animal.species!=='string'){
+    return false;
+  }
+  if(!animal.diet||typeof animal.diet!=='string'){
+    return false;
+  }
+  if(!animal.personalityTraits||typeof animal.personalityTraits=='string'){
+    return false;
+  }
+  else{
+    return true;
+  }
 }
 app.get('/api/animals',(req,res)=>{
   let results=animals;
@@ -44,6 +79,8 @@ app.get('/api/animals',(req,res)=>{
   }
   res.json(results);
 });
+
+
 app.get('/api/animals/:id',(req,res)=>{
   const result=findById(req.params.id,animals);
  if(result){
@@ -53,6 +90,20 @@ app.get('/api/animals/:id',(req,res)=>{
    res.send(404);
  }
 });
+
+
+app.post('/api/animals',(req,res)=>{
+  req.body.id=animals.length.toString();
+  if(!validateAnimal(req.body)){
+    res.status(400).send('The animal is not properly formatted.');
+  }
+  else{
+    const animal=createNewAnimal(req.body,animals);
+    res.json(animal);
+  }
+});
+
+
 app.listen(PORT,()=>{
   console.log(`API server now on port ${PORT}`);
 });
